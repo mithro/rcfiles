ddeec4fc7045d4b564da4ad83c73b2b4ee00672b
Tim 'mithro' Ansell <mithro@mithis.com>
2012-02-20 02:34:32 +0000

    Backup multiple users.

--- backup/backup-github.py
+++ backup/backup-github.py
@@ -12,12 +12,12 @@ root = os.path.realpath(root)
 
 os.chdir(root)
 
-user = "mithro"
-f = urllib2.urlopen("https://github.com/api/v2/json/repos/show/%s" % user)
-data = json.load(f)
-for repo in data['repositories']:
-	url = repo['url'].replace('https://', 'git+ssh://git@')
-	if not os.path.exists(repo['name'] + ".git"):
-		subprocess.call("git clone --bare %s" % url, shell=True)
-	else:
-		subprocess.call("cd %s.git; git fetch %s +refs/heads/*:+refs/heads/*" % (repo['name'], url), shell=True)
+for user in ["mithro", "timsvideo"]:
+	f = urllib2.urlopen("https://github.com/api/v2/json/repos/show/%s" % user)
+	data = json.load(f)
+	for repo in data['repositories']:
+		url = repo['url'].replace('https://', 'git+ssh://git@')
+		if not os.path.exists(repo['name'] + ".git"):
+			subprocess.call("git clone --bare %s" % url, shell=True)
+		else:
+			subprocess.call("cd %s.git; git fetch %s +refs/heads/*:+refs/heads/*" % (repo['name'], url), shell=True)
